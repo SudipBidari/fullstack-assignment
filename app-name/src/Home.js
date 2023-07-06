07dd7dc93c740774c4c5ef8c6f744aa576234c27
SudipBidari <spidun118@gmail.com>
2023-07-07 01:17:23 +0545

    upadated product.js

--- app-name/src/Home.js
+++ app-name/src/Home.js
@@ -10,7 +10,9 @@ function Home() {
   <img className="home_img" src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/34424535-c82f-42d2-9d73-d55105493f58/d5wommr-da40044c-62fc-43fa-8bcb-66960ca066d6.jpg/v1/fill/w_900,h_603,q_75,strp/reading_in_the_morning_by_ta_moe_d5wommr-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NjAzIiwicGF0aCI6IlwvZlwvMzQ0MjQ1MzUtYzgyZi00MmQyLTlkNzMtZDU1MTA1NDkzZjU4XC9kNXdvbW1yLWRhNDAwNDRjLTYyZmMtNDNmYS04YmNiLTY2OTYwY2EwNjZkNi5qcGciLCJ3aWR0aCI6Ijw9OTAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.JgSQ4OcvcqihKYc8hrRRXFudRXdlDloQJXYEr6de1rE"/>
 
   <div className="home_row">
-    <Product title='The Great Gatsby by F. Scott Fitzgerald: Gatsbys obsession to reunite with his former lover' 
+    <Product 
+    id={24001}
+    title='The Great Gatsby by F. Scott Fitzgerald: Gatsbys obsession to reunite with his former lover' 
     price={50.78} 
     image='https://upload.wikimedia.org/wikipedia/commons/7/7a/The_Great_Gatsby_Cover_1925_Retouched.jpg'
     rating={5}
@@ -22,24 +24,32 @@ function Home() {
     />
   </div>
   <div className="home_row">
-    <Product title='The Great Gatsby by F. Scott Fitzgerald: Gatsbys obsession to reunite with his former lover' 
+    <Product 
+    id={24002}
+    title='The Great Gatsby by F. Scott Fitzgerald: Gatsbys obsession to reunite with his former lover' 
     price={50.78} 
     image='https://upload.wikimedia.org/wikipedia/commons/7/7a/The_Great_Gatsby_Cover_1925_Retouched.jpg'
     rating={5}
     />
-    <Product title='The Great Gatsby by F. Scott Fitzgerald: Gatsbys obsession to reunite with his former lover' 
+    <Product 
+    id={24003}
+    title='The Great Gatsby by F. Scott Fitzgerald: Gatsbys obsession to reunite with his former lover' 
     price={50.78} 
     image='https://upload.wikimedia.org/wikipedia/commons/7/7a/The_Great_Gatsby_Cover_1925_Retouched.jpg'
     rating={5}
     />
-    <Product title='The Great Gatsby by F. Scott Fitzgerald: Gatsbys obsession to reunite with his former lover' 
+    <Product 
+    id={24004}
+    title='The Great Gatsby by F. Scott Fitzgerald: Gatsbys obsession to reunite with his former lover' 
     price={50.78} 
     image='https://upload.wikimedia.org/wikipedia/commons/7/7a/The_Great_Gatsby_Cover_1925_Retouched.jpg'
     rating={5}
     />
   </div>
   <div className="home_row">
-  <Product title='The Great Gatsby by F. Scott Fitzgerald: Gatsbys obsession to reunite with his former lover' 
+  <Product 
+    id={24005}
+    title='The Great Gatsby by F. Scott Fitzgerald: Gatsbys obsession to reunite with his former lover' 
     price={50.78} 
     image='https://upload.wikimedia.org/wikipedia/commons/7/7a/The_Great_Gatsby_Cover_1925_Retouched.jpg'
     rating={5}
--- app-name/src/Product.js
+++ app-name/src/Product.js
@@ -1,6 +1,20 @@
 import React from 'react';
 import "./Product.css";
-function product({title, image, price, rating}) {
+function product({id, title, image, price, rating}) {
+  const [state, dispatch] = useStateValue();
+  const addToBasket = () => {
+    //dispatch the item into the data layer
+    dispatch({
+      type: 'ADD_TO_BASKET'
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
   return (
     <div className='product'>
       <div className="product_info">
@@ -18,7 +32,7 @@ function product({title, image, price, rating}) {
       </div>
       <img 
       src={image} alt="" />
-      <button>Add to cart</button>
+      <button onClick={addToBasket}>Add to cart</button>
       
       </div>
   )
